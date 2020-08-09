27d2809e9e0913b7c9e778a861fc8d712112bf4a
David Jensenius <david@jensenius.com>
2020-08-09 16:45:42 -0400

    Production fixes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AdminController < ApplicationController
+class AdminController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   def index
