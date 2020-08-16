4180d101b302f7245e1386489ac0cd70dffacfa4
David Jensenius <david@jensenius.com>
2020-08-15 22:52:16 -0400

    Hotfix

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,6 @@ module CondoApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.time_zone = "Eastern Time (US & Canada)"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
