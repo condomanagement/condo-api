93fb46694a2393cf17bde6f39ef8eba78f8d9431
David Jensenius <david@jensenius.com>
2020-10-17 12:08:24 -0400

    Add some tests

--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -72,6 +72,19 @@ class ReservationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
   end
 
+  test "should not update reservation if missing data" do
+    patch reservation_url(@reservation), params:
+      {
+        reservation: {
+          resource_id: "ghost",
+          user_id: @reservation.user_id
+        }
+      }, headers: {
+        "HTTP_COOKIE" => "token=#{@token};"
+      }
+    assert_response :unprocessable_entity
+  end
+
   test "should not destroy reservation if unauthorized" do
     assert_difference("Reservation.count", 0) do
       delete reservation_url(@reservation)
@@ -99,11 +112,12 @@ class ReservationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "find reservations" do
+    @reservation.save
     post find_reservations_url, params:
       {
-        resource: 1,
+        resource: @reservation.id,
         startDay: "2020-01-01T04:00:00.000Z",
-        endDay: "2020-01-01T-4:30:00.000Z",
+        endDay: "2022-01-01T-4:30:00.000Z",
         controller: "reservations",
         action: "find_reservations",
         reservations: {}
@@ -111,5 +125,17 @@ class ReservationsControllerTest < ActionDispatch::IntegrationTest
         "HTTP_COOKIE" => "token=#{@token};"
       }
     assert_response :ok
+  end
+
+  test "find reservations fails" do
+    post find_reservations_url, params:
+      {
+        controller: "reservations",
+        action: "find_reservations",
+        reservations: {}
+      }, headers: {
+        "HTTP_COOKIE" => "token=#{@token};"
+      }
+    assert_response :unprocessable_entity
   end
 end
