3db300db87fae81b5ddb469aa4fa322ccbfae394
David Jensenius <david@jensenius.com>
2025-11-16 13:51:16 +0100

    Fix admin? method to use safe navigation properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -128,7 +128,7 @@ private
     return false unless request.cookies["token"]
 
     @authentication = Authentication.find_by(token: request.cookies["token"])
-    return true if @authentication.user.admin && @authentication&.user&.active
+    return true if @authentication&.user&.admin && @authentication&.user&.active
 
     false
   end
